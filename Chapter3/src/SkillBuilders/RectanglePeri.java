06722f1b112f8db488b86ffc7c76d795a79b7244
16335007 <16335007@L815WD220012.edu.cbe.ab.ca>
2024-09-20 10:25:08 -0600

    10:24AM

--- Chapter3/src/SkillBuilders/RectanglePeri.java
+++ Chapter3/src/SkillBuilders/RectanglePeri.java
@@ -5,8 +5,11 @@ public class RectanglePeri {
 	
 	public static void main(String[] args)
 	{
-	
-
+		int w = 4;
+		int l = 13;
+		System.out.print ("the perimiter of the rectangle is ");
+		System.out.print (2 * w + 2 * l);
+		
 	}
 
 }
